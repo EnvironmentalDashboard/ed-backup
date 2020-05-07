b39989de39ec809057605eee375441a3416d6eef
Sam Craig <craigsamm@gmail.com>
2020-05-07 18:40:50 -0400

    Working scheduler

--- server.rb
+++ server.rb
@@ -1,6 +1,16 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
+require "rufus-scheduler"
+
+configure do
+  set :scheduler, Rufus::Scheduler.new
+end
+
 get "/" do
-  "Hi!"
+  settings.scheduler.in '10s' do
+    File.write('test', Time.now)
+  end
+
+  "Hi!!!"
 end
